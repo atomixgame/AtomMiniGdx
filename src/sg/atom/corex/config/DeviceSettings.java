f45924988b5b3d8424887ea3073bff978d0cf73f
atomix <atomixnmc@gmail.com>
2015-10-01 10:36:24 +0700

    Add landscape mode

--- src/sg/atom/corex/config/DeviceSettings.java
+++ src/sg/atom/corex/config/DeviceSettings.java
@@ -5,14 +5,39 @@ package sg.atom.corex.config;
  * @author CuongNguyen
  */
 public enum DeviceSettings {
-    Phone(800,480), PC(1024,768);
+
+    Phone(800, 480), PC(1024, 768);
 
     private DeviceSettings(int width, int height) {
         this.width = width;
         this.height = height;
     }
-     
+    public boolean landscape;
     public int width;
     public int height;
-    
+
+    public int getWidth() {
+        if (landscape) {
+            return width;
+        } else {
+            return height;
+        }
+    }
+
+    public int getHeight() {
+        if (landscape) {
+            return height;
+        } else {
+            return width;
+        }
+    }
+
+    public void setLandscape(boolean landscape) {
+        this.landscape = landscape;
+    }
+
+    public boolean isLandscape() {
+        return landscape;
+    }
+
 }
